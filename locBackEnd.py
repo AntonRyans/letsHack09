43ded5bb7fdcfeaf9d550019930a9e16c6c848e0
Anton Ryans <114229477+AntonRyans@users.noreply.github.com>
2024-11-02 10:36:34 +0000

    Add files via upload

---
+++ locBackEnd.py
@@ -0,0 +1,64 @@
+# Importing Necessary Modules
+import requests
+from selenium import webdriver
+import folium
+import datetime
+import time
+
+# this method will return us our actual coordinates
+# using our ip address
+
+def locationCoordinates():
+    try:
+        response = requests.get('https://ipinfo.io')
+        data = response.json()
+        loc = data['loc'].split(',')
+        lat, long = float(loc[0]), float(loc[1])
+        city = data.get('city', 'Unknown')
+        state = data.get('region', 'Unknown')
+        return lat, long, city, state
+        # return lat, long
+    except:
+        # Displaying ther error message
+        print(&quot;Internet Not avialable&quot;)
+        # closing the program
+        exit()
+        return False
+
+
+# this method will fetch our coordinates and create a html file
+# of the map
+def gps_locator():
+
+    obj = folium.Map(location=[0, 0], zoom_start=2)
+
+    try:
+        lat, long, city, state = locationCoordinates()
+        print(quot;You Are in {},{}&quot;.format(city, state))
+        print(quot;Your latitude = {} and longitude = {}&quot;.format(lat, long))
+        folium.Marker([lat, long], popup='Current Location').add_to(obj)
+
+        fileName = &quot;C:/screengfg/Location&quot; + \
+            str(datetime.date.today()) + &quot;.html&quot;
+
+        obj.save(fileName)
+
+        return fileName
+
+    except:
+        return False
+
+
+# Main method
+if __name__ == &quot;__main__&quot;:
+
+    print(&quot;---------------GPS Using Python---------------\n&quot;)
+
+    # function Calling
+    page = gps_locator()
+    print(&quot;\nOpening File.............&quot;)
+    dr = webdriver.Chrome()
+    dr.get(page)
+    time.sleep(4)
+    dr.quit()
+    print(&quot;\nBrowser Closed..............&quot;)
